29e603196382647ba1753640cbd40eb5e0c69e44
JoaoPedro53 <jpsv32@gmail.com>
2026-03-01 23:52:21 -0300

    feat: initial commit

---
+++ src/main/java/jota/developer/SystemOrderUniformApplication.java
@@ -0,0 +1,13 @@
+package jota.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOrderUniformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemOrderUniformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jota/developer/SystemOrderUniformApplicationTests.java
@@ -0,0 +1,13 @@
+package jota.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOrderUniformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
